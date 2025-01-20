01ef49b3eafa4803898b540d6573bae4772fb88d
Sanjay Kumar <ersanjay426@gmail.com>
2025-01-20 23:08:35 +0530

    Group Anagrams

---
+++ array_and_hashing/GroupAnagrams.java
@@ -0,0 +1,45 @@
+package array_and_hashing;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GroupAnagrams {
+
+    public static List<List<String>> groupAnagramsBySorting(String[] strs) {
+        Map<String, List<String>> res = new HashMap<>();
+        for (String s : strs) {
+            char[] charArray = s.toCharArray();
+            Arrays.sort(charArray);
+            String sortedS = new String(charArray);
+            res.putIfAbsent(sortedS, new ArrayList<>());
+            res.get(sortedS).add(s);
+        }
+        return new ArrayList<>(res.values());
+    }
+
+    public static List<List<String>> groupAnagramsByHashMap(String[] strs) {
+        Map<String, List<String>> res = new HashMap<>();
+        for (String s : strs) {
+            int[] count = new int[26];
+            for (char c : s.toCharArray()) {
+                count[c - 'a']++;
+            }
+            String key = Arrays.toString(count);
+            res.putIfAbsent(key, new ArrayList<>());
+            res.get(key).add(s);
+        }
+        return new ArrayList<>(res.values());
+    }
+
+    public static void main(String[] args) {
+
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(groupAnagramsBySorting(strs));
+        System.out.println(groupAnagramsByHashMap(strs));
+        
+    }
+    
+}
