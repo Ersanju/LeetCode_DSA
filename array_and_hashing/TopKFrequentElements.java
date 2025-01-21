4d3645932a9d491bc12648105828a88f2b04102c
Sanjay Kumar <ersanjay426@gmail.com>
2025-01-21 23:27:29 +0530

    Top K frequent elements in an array

---
+++ array_and_hashing/TopKFrequentElements.java
@@ -0,0 +1,99 @@
+package array_and_hashing;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class TopKFrequentElements {
+
+    public int[] topKFrequentBySorting(int[] nums, int k) {
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+
+        List<int[]> arr = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+            arr.add(new int[] {entry.getValue(), entry.getKey()});
+        }
+        arr.sort((a, b) -> b[0] - a[0]);
+
+        int[] res = new int[k];
+        for (int i = 0; i < k; i++) {
+            res[i] = arr.get(i)[1];
+        }
+        return res;
+    }
+
+    public int[] topKFrequentByHeap(int[] nums, int k) {
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int num : nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+            heap.offer(new int[]{entry.getValue(), entry.getKey()});
+            if (heap.size() > k) {
+                heap.poll();
+            }
+        }
+
+        int[] res = new int[k];
+        for (int i = 0; i < k; i++) {
+            res[i] = heap.poll()[1];
+        }
+        return res;
+    }
+
+    public static int[] topKFrequentByBucketSort(int[] nums, int k) {
+        Map<Integer, Integer> count = new HashMap<>();
+        @SuppressWarnings("unchecked")
+        List<Integer>[] freq = new List[nums.length + 1];
+
+        for (int i = 0; i < freq.length; i++) {
+            freq[i] = new ArrayList<>();
+        }
+
+        for (int n : nums) {
+            count.put(n, count.getOrDefault(n, 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+            freq[entry.getValue()].add(entry.getKey());
+        }
+
+        int[] res = new int[k];
+        int index = 0;
+        for (int i = freq.length - 1; i > 0 && index < k; i--) {
+            for (int n : freq[i]) {
+                res[index++] = n;
+                if (index == k) {
+                    return res;
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1,2,2,2,2,3,3,3};
+        int k = 3;
+        int[] res1 = topKFrequentByBucketSort(nums, k);
+        for (int n : res1) {
+            System.out.print(n + " ");
+        }
+        int res2[] = new TopKFrequentElements().topKFrequentByHeap(nums, k);
+        for (int n : res2) {
+            System.out.print(n + " ");
+        }
+        int res3[] = new TopKFrequentElements().topKFrequentBySorting(nums, k);
+        for (int n : res3) {
+            System.out.print(n + " ");
+        }
+        
+    }
+    
+}
